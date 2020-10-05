5560709c28881fad48e4a5ec771cd260527590ec
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-10-05 21:17:05 +0200

    feat: handle focus change over multiple windows

--- background.js
+++ background.js
@@ -1,19 +1,24 @@
 
 const extId = 'mute-unfocused-tabs';
-let locked = new Set();
+let locked = {};
 
-async function onUpdated(tabId, changeInfo, tabInfo) { 
-	let tabs = await browser.tabs.query({active: true, currentWindow: true});
-	const atab = tabs[0];
+async function updateMuteState() {
+	let tabs = await browser.tabs.query({active: true, currentWindow:true});
+	const aid = tabs[0].id;
 	tabs = await browser.tabs.query({});
 	tabs.forEach( (tab) => {
 		if(typeof tab.id !== 'undefined') {
-			if( !locked.has(tab.id) ) {
-				if ( tab.id !== atab.id) {
+			if( typeof locked[tab.id] === 'boolean' ) {
+				browser.browserAction.setIcon({tabId: tab.id, path: "icon-locked.png"});
+				browser.tabs.update(tab.id, {muted: locked[tab.id]});
+			}else{
+				browser.browserAction.setIcon({tabId: tab.id, path: "icon.png"});
+				if ( tab.id !== aid) {
 					browser.tabs.update(tab.id, {muted: true});
 				}else{
 					browser.tabs.update(tab.id, {muted: false});
 				}
+
 			}
 		}
 	});
@@ -21,18 +26,26 @@ async function onUpdated(tabId, changeInfo, tabInfo) {
 
 async function onClicked(){
 	let tabs = await browser.tabs.query({active: true, currentWindow: true}); 
-	const atab = tabs[0];
-	if(locked.has(atab.id)){
-		locked.delete(atab.id);
-		//console.log(extId + 'unlocked', atab.id)
-		browser.browserAction.setIcon({"path": "icon.png"});
+	const aid = tabs[0].id;
+	if(typeof locked[aid] === 'boolean'){
+		delete locked[aid]
+		browser.browserAction.setIcon({tabId: aid, path: "icon.png"});
 	}else{
-		locked.add(atab.id);
-		//console.log(extId + 'locked', atab.id)
-		browser.browserAction.setIcon({"path": "icon-locked.png"});
+		locked[aid] = tabs[0].mutedInfo.muted;
+		browser.browserAction.setIcon({tabId: aid, path: "icon-locked.png"});
 	}
 }
 
-browser.browserAction.onClicked.addListener(onClicked);
-browser.tabs.onUpdated.addListener(onUpdated); 
+function onRemoved(tabId, removeInfo) {
+	if( typeof locked[tabId] === 'boolean') ) {
+		delete locked[tabId];
+	}
+}
 
+// add listeners
+browser.browserAction.onClicked.addListener(onClicked);
+browser.tabs.onActivated.addListener(updateMuteState); 
+browser.tabs.onUpdated.addListener(updateMuteState,/**/{properties: ["status"]}/**/); 
+browser.windows.onFocusChanged.addListener(updateMuteState);
+browser.runtime.onInstalled.addListener(updateMuteState);
+browser.tabs.onRemoved.addListener(onRemoved);
