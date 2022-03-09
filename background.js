402cfa6a037bfba14250d6f9ecef74d83c7c641b
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-03-09 10:39:43 +0100

    wip: only mute audible tabs

--- background.js
+++ background.js
@@ -13,6 +13,16 @@ const log = (level, msg) => {
 	}
 };
 
+function setMuted(tab, muted) {
+    if (!tab.mutedInfo.muted && tab.mutedInfo.reason === "user") {
+        // Never mute a tab that was manually unmuted.
+        return;
+    }
+    borwser.tabs.update(tab.id, {
+        muted
+    });
+}
+
 let unmanaged = new Set();
 
 function onRemoved(tabId, removeInfo) {
@@ -41,7 +51,7 @@ async function getWhitelisted() {
 		return [];
 	}
 
-	if ( typeof store.selectors.forEach !== 'function' ) { 
+	if ( typeof store.selectors.forEach !== 'function' ) {
 		log('error', 'store.selectors is not iterable');
 		return [];
 	}
@@ -54,12 +64,12 @@ async function getWhitelisted() {
 		if(typeof selector.activ !== 'boolean') { return; }
 		if(selector.activ !== true) { return; }
 
-		// check url regex 
+		// check url regex
 		if(typeof selector.url_regex !== 'string') { return; }
-		selector.url_regex = selector.url_regex.trim(); 
+		selector.url_regex = selector.url_regex.trim();
 		if(selector.url_regex === ''){ return; }
 
-		try { 
+		try {
 			wlist.push(new RegExp(selector.url_regex));
 		} catch(e) {
 			log('WARN', 'invalid url regex : ' + selectors.url_regex);
@@ -88,24 +98,61 @@ async function updateMuteState() {
 				if( !unmanaged.has(tab.id) ){
 					unmanaged.add(tab.id);
 				}
-				await browser.browserAction.setBadgeText({tabId: tab.id, text: "na" }); // forced unmanaged 
+				await browser.browserAction.setBadgeText({tabId: tab.id, text: "na" }); // forced unmanaged
 				skip = true;
 			}
 		}
 		if(!skip) {
 			if( unmanaged.has(tab.id) ) {
-				await browser.browserAction.setBadgeText({tabId: tab.id, text: "off" }); // unmanaged 
+				await browser.browserAction.setBadgeText({tabId: tab.id, text: "off" }); // unmanaged
 
 			}else{
-				await browser.tabs.update(tab.id, {muted: (tab.id !== aid)}); 
-				await browser.browserAction.setBadgeText({tabId: tab.id, text: "on" }); // managed 
+                if(tab.id === aid) {
+				    await browser.tabs.update(tab.id, {muted: false});
+                }else{
+                    if(tab.audible) {
+				        await browser.tabs.update(tab.id, {muted: true});
+                    }else{
+				        await browser.tabs.update(tab.id, {muted: false});
+                    }
+                }
+				await browser.browserAction.setBadgeText({tabId: tab.id, text: "on" }); // managed
 			}
 		}
 	});
 }
 
+
+async  function handleAudibleChange(tabId, changeInfo, tabInfo){
+
+	    let tabs = await browser.tabs.query({active: true, currentWindow: true});
+	    const aid = tabs[0].id;
+	    const wlist = await getWhitelisted();
+
+		let skip = false;
+		for (var i=0;i < wlist.length;i++) {
+			if(wlist[i].test(tabInfo.url)) {
+				if( !unmanaged.has(tabId) ){
+					unmanaged.add(tabId);
+				}
+				await browser.browserAction.setBadgeText({tabId: tabId, text: "na" }); // forced unmanaged
+				skip = true;
+			}
+		}
+		if(!skip) {
+			if( unmanaged.has(tabId) ) {
+				await browser.browserAction.setBadgeText({tabId: tabId, text: "off" }); // unmanaged
+
+			}else{
+				await browser.tabs.update(tabId, {muted: (tabId !== aid)});
+				await browser.browserAction.setBadgeText({tabId: tabId, text: "on" }); // managed
+			}
+		}
+
+}
+
 async function onClicked(){
-	
+
 	const tabs = await browser.tabs.query({active: true, currentWindow: true});
 	const atab = tabs[0];
 	const aid = atab.id;
@@ -121,7 +168,7 @@ async function onClicked(){
 // add listeners
 browser.browserAction.onClicked.addListener(onClicked);
 browser.tabs.onRemoved.addListener(onRemoved);
-browser.tabs.onActivated.addListener(updateMuteState); 
-//browser.tabs.onUpdated.addListener(updateMuteState,{properties:['status']}); 
+browser.tabs.onActivated.addListener(updateMuteState);
+browser.tabs.onUpdated.addListener(handleAudibleChange,{properties:['audible']});
 browser.windows.onFocusChanged.addListener(updateMuteState);
-browser.runtime.onInstalled.addListener(updateMuteState); 
+browser.runtime.onInstalled.addListener(updateMuteState);
